28d327be9c35dfd4fd39134714220bb143c8be79
TerrellTong <852507537@qq.com>
2020-04-15 20:57:41 +0800

    542.01 矩阵_广度优先遍历.java

---
+++ java/Day30题目/542.01 矩阵_广度优先遍历.java
@@ -0,0 +1,58 @@
+/*
+	算法思想：
+	  广度优先遍历，用队列
+	  
+				
+*/
+
+
+class Solution {
+    public int[][] updateMatrix(int[][] matrix) {
+		//因为是算1到0的距离，因此0入队
+        // 首先将所有的 0 都入队，并且将 1 的位置设置成 -1，表示该位置是 未被访问过的 1
+        //用队列来进行广度优先遍历
+        Queue<int[]> queue = new LinkedList<>();
+        int m = matrix.length, n = matrix[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    queue.offer(new int[] {i, j});
+                } else {
+                    matrix[i][j] = -1;
+                } 
+            }
+        }
+        
+        int[] dx = new int[] {-1, 1, 0, 0};
+        int[] dy = new int[] {0, 0, -1, 1};
+		//对队列进行处理
+        while (!queue.isEmpty()) {
+            int[] point = queue.poll();
+            int x = point[0], y = point[1];
+            for (int i = 0; i < 4; i++) {
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+                // 如果四邻域的点是 -1，表示这个点是未被访问过的 1
+                // 所以这个点到 0 的距离就可以更新成 matrix[x][y] + 1。
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n 
+                        && matrix[newX][newY] == -1) {
+                    matrix[newX][newY] = matrix[x][y] + 1;
+                    queue.offer(new int[] {newX, newY});
+                }
+            }
+        }
+
+        return matrix;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
