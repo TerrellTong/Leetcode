5e93172b1d9aa2f2311982836828276ff810deeb
TerrellTong <852507537@qq.com>
2020-04-17 20:34:43 +0800

    5.最长回文子串_动态规划.java

---
+++ java/5.最长回文子串_动态规划.java
@@ -0,0 +1,68 @@
+/*
+	算法思想：
+	 动态规划
+		状态转移：如果此时i,j相等则判断i+1,j-1是否是true/false
+
+
+链接：https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zhong-xin-kuo-san-dong-tai-gui-hua-by-liweiwei1419/
+*/
+
+
+public class Solution {
+
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        boolean[][] dp = new boolean[len][len];
+
+        // 初始化
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        int maxLen = 1;
+        int start = 0;
+
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+
+                if (s.charAt(i) == s.charAt(j)) {
+					//如果此时两个相等，而且j-i<3说明一定为回文了，
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        //状态转移
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                } else {
+                    dp[i][j] = false;
+                }
+
+                // 只要 dp[i][j] == true 成立，就表示子串 s[i, j] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j]) {
+                    int curLen = j - i + 1;
+                    if (curLen > maxLen) {
+                        maxLen = curLen;
+                        start = i;
+                    }
+                }
+            }
+        }
+        return s.substring(start, start + maxLen);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
