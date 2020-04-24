5bebc07a436b47b3538f81f0f682b63f54216c8e
TerrellTong <852507537@qq.com>
2020-04-24 20:38:12 +0800

    面试题51.数组中的逆序对_归并排序的思想.java

---
+++ java/Day30题目/面试题51.数组中的逆序对_归并排序的思想.java
@@ -0,0 +1,110 @@
+/*
+	算法思想：
+		归并排序
+*/
+
+
+public class Solution {
+
+    public int reversePairs(int[] nums) {
+        int len = nums.length;
+
+        if (len < 2) {
+            return 0;
+        }
+
+		//copy数组是为了让原数组不发生改变
+        int[] copy = new int[len];
+        for (int i = 0; i < len; i++) {
+            copy[i] = nums[i];
+        }
+
+        int[] temp = new int[len];
+        return reversePairs(copy, 0, len - 1, temp);
+    }
+
+    /**
+     * nums[left..right] 计算逆序对个数并且排序
+     *
+     * @param nums
+     * @param left
+     * @param right
+     * @param temp
+     * @return
+     */
+    private int reversePairs(int[] nums, int left, int right, int[] temp) {
+        if (left == right) {
+            return 0;
+        }
+
+        int mid = left + (right - left) / 2;
+        int leftPairs = reversePairs(nums, left, mid, temp);
+        int rightPairs = reversePairs(nums, mid + 1, right, temp);
+
+        // 如果整个数组已经有序，则无需合并，注意这里使用小于等于，此时已经划到了只有最小的两个数组
+        if (nums[mid] <= nums[mid + 1]) {
+            return leftPairs + rightPairs;
+        }
+
+        int crossPairs = mergeAndCount(nums, left, mid, right, temp);
+        return leftPairs + rightPairs + crossPairs;
+    }
+
+    /**
+     * nums[left..mid] 有序，nums[mid + 1..right] 有序
+     *
+     * @param nums
+     * @param left
+     * @param mid
+     * @param right
+     * @param temp
+     * @return
+     */
+    private int mergeAndCount(int[] nums, int left, int mid, int right, int[] temp) {
+        for (int i = left; i <= right; i++) {
+            temp[i] = nums[i];
+        }
+		
+		//i是数组的左指针，只能指到mid,即mid+1就不能移动i了
+		//j是数组的右指针，只能指到最尾部，如果指到了最尾部则就不能移动i
+        int i = left;
+        int j = mid + 1;
+
+        int count = 0;
+        for (int k = left; k <= right; k++) {
+            // 有下标访问，得先判断是否越界
+            if (i == mid + 1) {
+                nums[k] = temp[j];
+                j++;
+            } else if (j == right + 1) {
+                nums[k] = temp[i];
+                i++;
+            } else if (temp[i] <= temp[j]) {
+                // 注意：这里是 <= ，写成 < 就不对，请思考原因
+                nums[k] = temp[i];
+                i++;
+            } else {
+                nums[k] = temp[j];
+                j++;
+                
+                // 在 j 指向的元素归并回去的时候，计算逆序对的个数，只多了这一行代码,因为未归并回去，说明其余的都满足逆序的条件
+				//可以见视频
+                count += (mid - i + 1);
+            }
+        }
+        return count;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
