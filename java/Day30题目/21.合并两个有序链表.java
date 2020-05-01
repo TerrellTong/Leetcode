e3918f90ae70ab76dd6e9c98cf1e0433138bec0b
TerrellTong <852507537@qq.com>
2020-05-01 20:12:03 +0800

    21.合并两个有序链表.java

---
+++ java/Day30题目/21.合并两个有序链表.java
@@ -0,0 +1,49 @@
+/*
+	算法思想：
+		简单合并链表
+*/
+ 
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                tail.next = l1;
+                l1 = l1.next;
+            }else{
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        if(l1 != null){
+            tail.next = l1;
+        }
+        if(l2 != null){
+            tail.next = l2;
+        }
+        return head.next;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
