811c5124c886eb2616eab8142c4934b2d877ffbb
TerrellTong <852507537@qq.com>
2020-05-17 21:44:43 +0800

    25.K\ 个一组翻转链表表_纯粹链表操作.java

---
+++ java/Day30题目/25.K 个一组翻转链表_纯粹链表操作.java
@@ -0,0 +1,83 @@
+/*
+	算法思想：
+		拓扑排序的实现，BFS
+*/
+ 
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public ListNode reverseKGroup(ListNode head, int k) {
+      if (head == null || k == 1) {
+        return head;
+      }
+      ListNode dummy = new ListNode(0);
+      dummy.next = head;
+
+      ListNode start = dummy;
+      ListNode end = head;
+      int count = 0;
+      while (end != null) {
+        count++;
+        // group
+        if (count % k == 0) {
+          // reverse linked list (start, end]
+          start = reverse(start, end.next);
+          end = start.next;
+        } else {
+          end = end.next;
+        }
+      }
+      return dummy.next;
+    }
+
+    /**
+     * reverse linked list from range (start, end), return last node.
+     * for example:
+     * 0->1->2->3->4->5->6->7->8
+     * |           |
+     * start       end
+     *
+     * After call start = reverse(start, end)
+     *
+     * 0->3->2->1->4->5->6->7->8
+     *          |  |
+     *       start end
+     *       first
+     *
+     */
+    private ListNode reverse(ListNode start, ListNode end) {
+      ListNode curr = start.next;
+      ListNode prev = start;
+      ListNode first = curr;
+      //直接原地进行置换，不需要声明一个新的结点再用头插
+      while (curr != end){
+        ListNode temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp;
+      }
+      start.next = prev;
+      first.next = curr;
+      //这时的first相当于第一次dummy结点（0）
+      return first;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
