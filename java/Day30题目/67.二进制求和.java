f80645a82a4108d1f5af268aeb610afdaa8f66ff
TerrellTong <852507537@qq.com>
2020-06-23 20:09:13 +0800

    67.二进制求和.java

---
+++ java/Day30题目/67.二进制求和.java
@@ -0,0 +1,39 @@
+/*
+	算法思想：
+		先从尾指针开始计算，计算后再reverse
+		不是先reverse再计算（新思路）
+	第二次做：知道了与‘0’相减得到结果，要进行逆转，但还是未做出来，flag不知道怎么处理！
+	
+*/
+class Solution {
+    public String addBinary(String a, String b) {
+        char[] a_str = a.toCharArray();
+        char[] b_str = b.toCharArray();
+        //用StringBuilder来保存加后的值，然后进行逆转
+        StringBuilder sb = new StringBuilder();
+        //进位标志
+        int flag = 0;
+        //逆序
+        for(int i=a_str.length-1,j=b_str.length-1;i>=0||j>=0;i--,j--){
+            //将字符与0的ASCII码进行相减，结果就是int型，判断是否大于0
+            //>=0，用于判断是否会出现字符串越界，如果出现了就将该值设为0
+            int a_num = (i >= 0)?a_str[i]-'0' : 0;
+            int b_num = (j >= 0)?b_str[j]-'0' : 0;
+            int result = a_num+b_num+flag;
+            if(result >= 2){
+                sb.append(result - 2);
+                flag = 1;
+            }else{
+                sb.append(result);
+                flag = 0;
+            }
+        }
+        if(flag == 1)
+            sb.append(flag);
+        return sb.reverse()+"";
+    }
+}
+
+
+
+
