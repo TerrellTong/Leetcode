1dade97e6b37f99dadc6781d5084787792c7c038
TerrellTong <terrell_tong@126.com>
2020-07-27 21:05:03 +0800

    329.矩阵中的最长递增路径_深度优先搜索（困难）.java

--- java/Day30题目/329.矩阵中的最长递增路径_深度优先搜索（困难）.java
+++ java/Day30题目/329.矩阵中的最长递增路径_深度优先搜索（困难）.java
@@ -2,46 +2,46 @@
 	算法思想：
 		深度优先遍历
 
-	不是一个普通的dfs，因为要找到当前的递增路径，因此在判断的时候，以此为这个点为基础，找到四周是否存在比当前小的点，如果存在则进行以此点为基础进行深度优先搜索
-	这样存在的一个路径一定是一个递增/递减的， 但是递减从反方向看也是递增的！
-	相当于找一个递减路径！
+	只需要让下一步的点 > 当前点的值，则进行下一步递归
 
-https://leetcode-cn.com/problems/longest-increasing-path-in-a-matrix/solution/javashi-xian-shen-du-you-xian-chao-ji-jian-dan-yi-/
+https://leetcode-cn.com/problems/longest-increasing-path-in-a-matrix/solution/ju-zhen-zhong-de-zui-chang-di-zeng-lu-jing-by-le-2/
 */
 
 class Solution {
+    //路径步骤
+    public int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    public int rows,cols;
     public int longestIncreasingPath(int[][] matrix) {
         if(matrix.length == 0)
             return 0;
-        //声明visited数组，两个作用：判断是否访问过，存储当前格子的最长递增长度
-        int[][] visited = new int[matrix.length][matrix[0].length];
+        rows = matrix.length;
+        cols = matrix[0].length;
+        //声明visited数组的作用:能够判断是否访问了以及记录最长的递增路径
+        int[][] visited = new int[rows][cols];
+        //记录最长的递增路径长度
         int max = 0;
-        for(int i=0;i<matrix.length;i++)
-            for(int j=0;j<matrix[0].length;j++){
+        //对每一个单元格都要分别进行深度优先搜索
+        for(int i=0;i<rows;i++)
+            for(int j=0;j<cols;j++)
+                //如果当前数组未被遍历，则进行递归
                 if(visited[i][j] == 0)
                     max = Math.max(max,dfs(i,j,matrix,visited));
-            }
         return max;
     }
 
     public int dfs(int i,int j,int[][] matrix,int[][] visited){
-        if(i<0 || i>=matrix.length || j<0 || j>=matrix[0].length)
-            return 0;
         //进行记忆化
         if(visited[i][j] > 0)
             return visited[i][j];
-        int max = 0;
-        //去判断四周是否比当前数小，然后进行递归遍历
-        if(i-1 >= 0 && matrix[i-1][j] < matrix[i][j])
-            max = Math.max(max,dfs(i-1,j,matrix,visited));
-        if(i+1 < matrix.length && matrix[i+1][j] < matrix[i][j])
-            max = Math.max(max,dfs(i+1,j,matrix,visited));
-        if(j-1 >= 0 && matrix[i][j-1] < matrix[i][j])
-            max = Math.max(max,dfs(i,j-1,matrix,visited));
-        if(j+1 < matrix[0].length && matrix[i][j+1] < matrix[i][j])
-            max = Math.max(max,dfs(i,j+1,matrix,visited));
-        
-        visited[i][j] = max+1;
-        return max+1;
+        //如果进行了dfs，说明此时visit[i][j]的值++，说明此时路径++
+        visited[i][j]++;
+        for(int[] dir:dirs){
+            int newRow = i + dir[0], newColumn = j + dir[1];
+            //只需要下一步点的值，> 当前点的值，则进行下一步操作
+            if (newRow >= 0 && newRow < rows && newColumn >= 0 && newColumn < cols && matrix[newRow][newColumn] > matrix[i][j])
+				//dfs(newRow, newColumn,matrix, visited) + 1 表明当前所代表的数组，路径长度+1
+                visited[i][j] = Math.max(visited[i][j], dfs(newRow, newColumn,matrix, visited) + 1);
+        }
+        return visited[i][j];
     }
 }
