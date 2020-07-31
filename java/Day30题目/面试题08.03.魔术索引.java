540fc8dce8c19fb81b62444b89f99571ba9bd377
TerrellTong <terrell_tong@126.com>
2020-07-31 19:43:53 +0800

    面试题08.03.魔术索引.java

---
+++ java/Day30题目/面试题08.03.魔术索引.java
@@ -0,0 +1,14 @@
+/*
+	算法思想：
+		言简意赅！
+*/
+
+class Solution {
+    public int findMagicIndex(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == i)
+                return i;
+        }
+        return -1;
+    }
+}
