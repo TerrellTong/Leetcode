a5da64a2846bc433c8a5635125e146bba669a625
TerrellTong <terrell_tong@126.com>
2020-10-13 21:03:54 +0800

    84.柱状图中最大的矩形_单调栈.java

--- java/Day30题目/84.柱状图中最大的矩形_单调栈.java
+++ java/Day30题目/84.柱状图中最大的矩形_单调栈.java
@@ -17,38 +17,62 @@
 
 
 	33.当元素出栈后，说明新栈顶元素是出栈元素向前找第一个比其小的元素
+	
+	解法四：分别求出包含每个柱子的矩形区域的最大面积，然后选最大的。要包含这个柱子，也就是这个柱子是当前矩形区域的高度。
+			也就是，这个柱子是当前矩形区域中柱子最高的。如下图中包含橙色柱子的矩形区域的最大面积。
+			求当前的矩形区域，我们只需要知道比当前柱子到左边第一个小的 leftLessMin 和到右边第一个小的 rightLessMin 两个柱子下标，
+			就可以求出矩形的面积为 (rightLessMin - leftLessMin - 1) * 当前柱子高度。然后遍历每个柱子，按同样的方法求出矩形面积，选最大的就可以了。
 
+原理：https://leetcode-cn.com/problems/largest-rectangle-in-histogram/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-1-7/
 	
 	
 */
  
-class Solution {
-    public int largestRectangleArea(int[] heights) {
-        //返回的结果值
-        int res = 0;
-        int[] new_heights = new int[heights.length+2];
-        //声明一个栈
-        Deque<Integer> stack = new ArrayDeque();
-        //将heights转移到new_heights中，实现一一对应
-        for(int i=1;i<heights.length+1;i++)
-            new_heights[i] = heights[i-1];
-
-        //开始遍历
-        for(int i=0;i<new_heights.length;i++){
-            //如果栈不空，并且栈顶的元素大于此时的height[i]值，就进行出栈，然后进行面积的计算
-            while(!stack.isEmpty() && new_heights[stack.peek()] > new_heights[i]){
-                int cur = stack.pop();
-				//矩形的面积是，当前cur的高度 * 宽
-                res = Math.max(res,(i - stack.peek() - 1)*new_heights[cur]);
+public int largestRectangleArea(int[] heights) {
+    int maxArea = 0;
+    Stack<Integer> stack = new Stack<>();
+    int p = 0;
+    while (p < heights.length) {
+        //栈空入栈
+        if (stack.isEmpty()) {
+            stack.push(p);
+            p++;
+        } else {
+            int top = stack.peek();
+            //当前高度大于栈顶，入栈
+            if (heights[p] >= heights[top]) {
+                stack.push(p);
+                p++;
+            } else {
+                //保存栈顶高度
+                int height = heights[stack.pop()];
+                //左边第一个小于当前柱子的下标
+                int leftLessMin = stack.isEmpty() ? -1 : stack.peek();
+                //右边第一个小于当前柱子的下标
+                int RightLessMin = p;
+                //计算面积
+                int area = (RightLessMin - leftLessMin - 1) * height;
+                maxArea = Math.max(area, maxArea);
             }
-            //不管如何，都要入栈，
-            //在经过上面的操作之后，相当于比height[i]大的值已经都出栈了，此时的栈还是个单调递增的栈
-            //记录数组下标，他们实际对应的值是单调上升的
-            stack.push(i);
         }
-        return res;
     }
+    while (!stack.isEmpty()) {
+        //保存栈顶高度
+        int height = heights[stack.pop()];
+        //左边第一个小于当前柱子的下标
+        int leftLessMin = stack.isEmpty() ? -1 : stack.peek();
+        //右边没有小于当前高度的柱子，所以赋值为数组的长度便于计算
+        int RightLessMin = heights.length;
+        int area = (RightLessMin - leftLessMin - 1) * height;
+        maxArea = Math.max(area, maxArea);
+    }
+    return maxArea;
 }
+
+作者：windliang
+链接：https://leetcode-cn.com/problems/largest-rectangle-in-histogram/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-1-7/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 
 
