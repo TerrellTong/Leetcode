8c9b6f631484a48b83c54a0ca7d8c7349e0c1a96
TerrellTong <terrell_tong@126.com>
2020-10-26 19:15:14 +0800

    1365.有多少小于当前数字的数字.java

---
+++ java/Day30题目/1365.有多少小于当前数字的数字.java
@@ -0,0 +1,24 @@
+/*
+	算法思想：
+		暴力
+
+*/
+
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] res = new int[nums.length];
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            count = 0;
+            for(int j=0;j<nums.length;j++){
+                if(i != j){
+                    if(nums[j] < nums[i]){
+                        count++;
+                    }
+                }
+            }
+            res[i] = count;
+        }
+        return res;
+    }
+}
