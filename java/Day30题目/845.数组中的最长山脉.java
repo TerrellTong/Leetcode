081164394c1c43347a0e25d4aa827e01f6965c2a
TerrellTong <terrell_tong@126.com>
2020-11-03 19:50:52 +0800

    845.数组中的最长山脉.java

--- java/Day30题目/845.数组中的最长山脉.java
+++ java/Day30题目/845.数组中的最长山脉.java
@@ -7,28 +7,40 @@
 
 class Solution {
     public int longestMountain(int[] A) {
-        int ans = 0,n = A.length;
-
+        // 如果写到这里，当出现爬上坡，下坡，上坡时，会出现死循环，因此当成功进行一次上坡，下坡时，要置为0,而且出现过一次up>0,down=0，
+        // 我们还要重新计数，将up,down置为0！！不然会出现死循环
+        // int up = 0;
+        // int down = 0;
         int i = 0;
-        while(i < n){
-            int up = 0,down = 0;
-            while(i < n-1 && A[i] < A[i+1]){
-                i++;
+        int len = A.length;
+        int ans = 0;
+        while(i < len){
+            //每一轮企图上山前，都要将up,down 置为0！！！
+            //不能出现上山前，up,down 中任意一个非0
+            int up = 0 , down = 0;
+            //一直向山上爬
+            while(i < len-1 && A[i] < A[i+1]){
                 up++;
+                i++;
+                continue;
             }
+            //如果成功上山，那么就可以判断下山了
             if(up > 0){
-                while(i < n-1 && A[i] > A[i+1]){
-                    i++;
+                while(i < len-1 && A[i] > A[i+1]){
                     down++;
+                    i++;
+                    continue;
                 }
             }
-            //同时完成了，上坡和下坡，说明找到了山脉，更新答案
-            if(up > 0 && down > 0){
-                ans = Math.max(ans,up+down+1);
-            }
-            //爬坡失败
-            if(up == 0)
+
+            //如果上山失败，则移动i指针
+            if(up == 0){
                 i++;
+            }
+
+            if(up > 0 && down > 0){
+                ans = Math.max(up+down+1,ans);
+            }
         }
         return ans;
     }
