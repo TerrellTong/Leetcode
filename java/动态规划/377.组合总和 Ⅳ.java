d26693908644009b3a15bc0144a0f70db654316d
TerrellTong <terrell_tong@126.com>
2020-11-12 20:17:51 +0800

    377.组合总和 Ⅳ.java

---
+++ java/动态规划/377.组合总和 Ⅳ.java
@@ -0,0 +1,68 @@
+/*
+	算法思想：
+		记忆化搜索
+		
+题解： https://leetcode-cn.com/problems/combination-sum-iv/solution/ti-jie-yi-se-de-dong-tai-gui-hua-an-lai-ge-dfsde-b/ （java）
+		为什么target== 0 要返回1 ？
+		https://leetcode-cn.com/problems/combination-sum-iv/solution/dong-tai-gui-hua-python-dai-ma-by-liweiwei1419/
+*/
+
+class Solution {
+    int[] memo;
+    public int combinationSum4(int[] nums, int target) {
+        memo = new int[target+1];
+        Arrays.fill(memo,-1);
+        return tryCombination(nums,target);
+    }
+
+    public int tryCombination(int[] nums,int target){
+        if(target == 0)
+            return 1;
+
+        //此时不能再次进行拆分了
+        if(target < 0)
+            return -1;
+
+        if(memo[target] != -1)
+            return memo[target];
+
+        int res = 0;
+        for(int i=0;i<nums.length;i++){
+            //判断当前数-nums[i]后所需要的次数
+            int tmp = tryCombination(nums,target-nums[i]);
+            //tmp != -1说明可以进行相应的拆分
+            if(tmp != -1)
+                res += tmp;
+        }
+
+        memo[target] = res;
+        return memo[target];
+    }
+}
+
+//回溯，超时！！！
+class Solution {
+    List<List<Integer>> list = new ArrayList();
+    public int combinationSum4(int[] nums, int target) {
+        dfs(nums,target,new ArrayList(),0);
+        return list.size();
+    }
+
+    public void dfs(int[] nums,int target,List<Integer> cur,int sum){
+        if(sum == target){
+            list.add(new ArrayList(cur));
+            return;
+        }
+
+        if(sum >target)
+            return;
+
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
+            cur.add(nums[i]);
+            dfs(nums,target,cur,sum);
+            cur.remove(cur.size()-1);
+            sum -= nums[i];
+        }
+    }
+}
