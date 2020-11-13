ed7bfde96f1a5d7cc87cb944a7a70c4489d39008
TerrellTong <terrell_tong@126.com>
2020-11-13 11:39:34 +0800

    377.组合总和 Ⅳ.java

--- java/动态规划/377.组合总和 Ⅳ.java
+++ java/动态规划/377.组合总和 Ⅳ.java
@@ -2,6 +2,8 @@
 	算法思想：
 		记忆化搜索
 		
+		这题是找到组合数，与之前的问题不同，之前做的都是比较选择最小，这里是要求累加！！！
+		
 题解： https://leetcode-cn.com/problems/combination-sum-iv/solution/ti-jie-yi-se-de-dong-tai-gui-hua-an-lai-ge-dfsde-b/ （java）
 		为什么target== 0 要返回1 ？
 		https://leetcode-cn.com/problems/combination-sum-iv/solution/dong-tai-gui-hua-python-dai-ma-by-liweiwei1419/
@@ -20,6 +22,7 @@ class Solution {
             return 1;
 
         //此时不能再次进行拆分了
+		//注意这种不能继续拆分的情况的做法！！！
         if(target < 0)
             return -1;
 
@@ -31,6 +34,8 @@ class Solution {
             //判断当前数-nums[i]后所需要的次数
             int tmp = tryCombination(nums,target-nums[i]);
             //tmp != -1说明可以进行相应的拆分
+			//这题是找到组合数，与之前的问题不同，之前做的都是比较选择最小，这里是要求累加！！！新题型get!!
+			//注意要判断-1，因为为-1，说明此时的方案是行不通的！！
             if(tmp != -1)
                 res += tmp;
         }
