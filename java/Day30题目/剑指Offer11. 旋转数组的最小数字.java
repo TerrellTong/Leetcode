503ad9839aee6c51002ff5c1ad23ada8e7fb89aa
TerrellTong <terrell_tong@126.com>
2020-12-15 20:41:54 +0800

    剑指Offer11. 旋转数组的最小数字.java

--- java/Day30题目/剑指Offer11. 旋转数组的最小数字.java
+++ java/Day30题目/剑指Offer11. 旋转数组的最小数字.java
@@ -30,3 +30,39 @@ class Solution {
         return numbers[i];
     }
 }
+
+
+
+https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/solution/er-fen-jian-zhi-si-xiang-fen-zhi-si-xiang-by-liwei/
+
+public int minArray(int[] numbers) {
+        int len = numbers.length;
+        if (len == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = len - 1;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (numbers[mid] > numbers[right]) {
+                // [3, 4, 5, 1, 2]，mid 以及 mid 的左边一定不是最小数字
+                // 下一轮搜索区间是 [mid + 1, right]
+                left = mid + 1;
+            } else if (numbers[mid] == numbers[right]) {
+                // 只能把 right 排除掉，下一轮搜索区间是 [left, right - 1]
+                right = right - 1;
+            } else {
+                // 此时 numbers[mid] < numbers[right]
+                // mid 的右边一定不是最小数字，mid 有可能是，下一轮搜索区间是 [left, mid]
+                right = mid;
+            }
+        }
+
+        // 最小数字一定在数组中，因此不用后处理
+        return numbers[left];
+    }
+
+作者：liweiwei1419
+链接：https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/solution/er-fen-jian-zhi-si-xiang-fen-zhi-si-xiang-by-liwei/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
