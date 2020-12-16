dd4ca71aa93233e36c7beebe5c1be6b6eeb1449c
TerrellTong <terrell_tong@126.com>
2020-12-16 11:38:52 +0800

    738.单调递增的数字_贪心.java

--- java/Day30题目/738.单调递增的数字_贪心.java
+++ java/Day30题目/738.单调递增的数字_贪心.java
@@ -12,20 +12,25 @@
 
 class Solution {
     public int monotoneIncreasingDigits(int N) {
-        char[] strN = Integer.toString(N).toCharArray();
-        int i=1;
-        //找到第一个不满足递增的位置
-        while(i < strN.length && strN[i-1] <= strN[i]){
+        //将数字转成字符数组
+        char[] n_arr = String.valueOf(N).toCharArray();
+        int i = 1;
+        //找到第一个不是递增的位置
+        while(i < n_arr.length && n_arr[i-1] <= n_arr[i])
             i++;
-        }
-        if(i < strN.length){
-            while(i > 0 && strN[i-1] > strN[i]){
-                strN[i-1] -= 1;
+        if(i < n_arr.length){
+            while(i >= 1 && n_arr[i-1] > n_arr[i]){
+                //高位-1
+                n_arr[i-1] -= 1;
+                //只将高位-1不能够，因为如果遇到339这样的，只对第二个3减1后，发现还是不满足条件
+                //因此，我们要一直像这样循环，知道n_arr[i-1] < n_arr[i] 是递增的！！（我没考虑到的情况）
                 i--;
             }
-            for(i = i+1;i<strN.length;i++)
-                strN[i] = '9';
+            //此时的i-1,i是递增的，我们将i后面的全部赋值为9
+            for(i = i+1;i<n_arr.length;i++)
+                n_arr[i] = '9';
         }
-        return Integer.valueOf(new String(new String(strN)));
+        //根据字符数组创建字符串
+        return Integer.valueOf(new String(n_arr));
     }
 }
