803cc723269bdefbaffd69b3e8cb068496ea8021
TerrellTong <terrell_tong@126.com>
2020-12-28 10:36:55 +0800

    面试题51.数组中的逆序对_归并排序的思想.java

--- java/Day30题目/面试题51.数组中的逆序对_归并排序的思想.java
+++ java/Day30题目/面试题51.数组中的逆序对_归并排序的思想.java
@@ -1,97 +1,55 @@
 /*
 	算法思想：
-		归并排序
+		归并排序，按照归并排序的模板写的
+		
+题解：https://leetcode-cn.com/problems/shu-zu-zhong-de-ni-xu-dui-lcof/solution/java-gui-bing-fa-by-1yx/  评论区
 */
 
 
-public class Solution {
-
+class Solution {
     public int reversePairs(int[] nums) {
-        int len = nums.length;
-
-        if (len < 2) {
-            return 0;
-        }
-
-		//copy数组是为了让原数组不发生改变
-        int[] copy = new int[len];
-        for (int i = 0; i < len; i++) {
-            copy[i] = nums[i];
-        }
-
-        int[] temp = new int[len];
-        return reversePairs(copy, 0, len - 1, temp);
+        int[] tmp = new int[nums.length];
+        return merger(nums,0,nums.length-1,tmp);
     }
 
-    /**
-     * nums[left..right] 计算逆序对个数并且排序
-     *
-     * @param nums
-     * @param left
-     * @param right
-     * @param temp
-     * @return
-     */
-    private int reversePairs(int[] nums, int left, int right, int[] temp) {
-        if (left == right) {
-            return 0;
+    public int merger(int[] nums,int low,int high,int[] tmp){
+        if(low < high){
+            int mid = (low + high) >>> 1;
+            int left = merger(nums,low,mid,tmp);
+            int right = merger(nums,mid+1,high,tmp);
+            
+            int cross = mersort(nums,low,mid,high,tmp);
+            return left+cross+right;
         }
-
-        int mid = left + (right - left) / 2;
-        int leftPairs = reversePairs(nums, left, mid, temp);
-        int rightPairs = reversePairs(nums, mid + 1, right, temp);
-
-        // 如果整个数组已经有序，则无需合并，注意这里使用小于等于，此时已经划到了只有最小的两个数组
-        if (nums[mid] <= nums[mid + 1]) {
-            return leftPairs + rightPairs;
-        }
-
-        int crossPairs = mergeAndCount(nums, left, mid, right, temp);
-        return leftPairs + rightPairs + crossPairs;
+        return 0;
     }
 
-    /**
-     * nums[left..mid] 有序，nums[mid + 1..right] 有序
-     *
-     * @param nums
-     * @param left
-     * @param mid
-     * @param right
-     * @param temp
-     * @return
-     */
-    private int mergeAndCount(int[] nums, int left, int mid, int right, int[] temp) {
-        for (int i = left; i <= right; i++) {
-            temp[i] = nums[i];
-        }
-		
-		//i是数组的左指针，只能指到mid,即mid+1就不能移动i了
-		//j是数组的右指针，只能指到最尾部，如果指到了最尾部则就不能移动i
-        int i = left;
-        int j = mid + 1;
-
+    public int mersort(int[] nums,int low,int mid,int high,int[] tmp){
+        int index = 0;
+        int i = low;
+        int j = mid+1;
         int count = 0;
-        for (int k = left; k <= right; k++) {
-            // 有下标访问，得先判断是否越界
-            if (i == mid + 1) {
-                nums[k] = temp[j];
-                j++;
-            } else if (j == right + 1) {
-                nums[k] = temp[i];
-                i++;
-            } else if (temp[i] <= temp[j]) {
-                // 注意：这里是 <= ，写成 < 就不对，请思考原因
-                nums[k] = temp[i];
-                i++;
-            } else {
-                nums[k] = temp[j];
-                j++;
+        while(i <= mid && j<=high){
+            //一定要取等于，不然的话，当出现重复元素，还会重复计算
+            if(nums[i] <= nums[j]){
                 
-                // 在 j 指向的元素归并回去的时候，计算逆序对的个数，只多了这一行代码,因为未归并回去，说明其余的都满足逆序的条件
-				//可以见视频
-                count += (mid - i + 1);
+                tmp[index++] = nums[i++];
+            }else{
+                //出现逆序对
+                count += mid+1 - i;
+                tmp[index++] = nums[j++];
             }
         }
+
+        while(i<=mid){
+            tmp[index++] = nums[i++];
+        }
+        while(j <= high){
+            tmp[index++] = nums[j++];
+
+        }
+        for(int t=0;t<index;t++)
+            nums[low+t] = tmp[t];
         return count;
     }
 }
