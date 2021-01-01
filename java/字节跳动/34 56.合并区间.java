8e06d3c47aee53297afe9d1f59d63c97d3487efb
TerrellTong <terrell_tong@126.com>
2021-01-01 16:35:13 +0800

    34 56.合并区间.java

--- java/字节跳动/34 56.合并区间.java
+++ java/字节跳动/34 56.合并区间.java
@@ -3,6 +3,8 @@
 		用res存储，每次比较，取res的值的左右区间即可
 		之前做过，跟合并区间弄混了！！！（没做出来！！）
 		
+		常规的区间问题一般都是通过这样的思路进行解决，就是先根据起点进行排序，然后用一个指针获取前一个区间，用于比较，再遍历数组，。然后处理重叠的情况！！
+		
 题解：https://leetcode-cn.com/problems/merge-intervals/solution/tan-xin-suan-fa-java-by-liweiwei1419-3/
 */
 
@@ -34,5 +36,48 @@ class Solution {
         }
 		//将res转成二维数组！
         return res.toArray(new int[res.size()][]);
+    }
+}
+
+//按照新的思路做的
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        //排序
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a,int[] b){
+                //按照从最左边排序
+                return a[0]-b[0];
+            }
+        });
+        //将intervals[0]，放入List中（list存储的就是合并区间后的结果）
+        List<int[]> res = new ArrayList();
+         //left，right为待插入的区间左右值
+        int left = intervals[0][0];
+        int right = intervals[0][1];
+
+        for(int i=1;i<intervals.length;i++){
+            //获取当前区间的值
+            int curLeft = intervals[i][0];
+            int curRight = intervals[i][1];
+
+            //如果当前left大于上一个最右边，将上一个区间加入res
+            if(curLeft > right){
+                res.add(new int[]{left,right});
+                left = curLeft;
+                right = curRight;
+            }else if(curRight < left){
+                //此时当前left大于于list的最右边，可以合并
+                res.add(new int[]{left,right});
+                left = curLeft;
+                right = curRight;
+            }else{
+                // 此时存在重合
+                left = Math.min(left,curLeft);
+                right = Math.max(right,curRight);
+            }
+        }
+        res.add(new int[]{left,right});
+        return res.toArray(new int[0][]);
     }
 }
