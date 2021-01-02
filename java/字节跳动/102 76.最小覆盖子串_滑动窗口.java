c8f011bbcd0fa6f4cddb35233f4e771be55faf3f
TerrellTong <terrell_tong@126.com>
2021-01-02 21:11:42 +0800

    102 76.最小覆盖子串_滑动窗口.java

--- java/字节跳动/102 76.最小覆盖子串_滑动窗口.java
+++ java/字节跳动/102 76.最小覆盖子串_滑动窗口.java
@@ -1,48 +1,58 @@
 /*
 	算法思想：
-		无法暴力AC（已经尝试）
-		采用双端队列，用双端队列维护一个单调递减的值（队头最大，队尾最小）
-		双端队列存放下标，从而还可以通过下标值来判断当前下标值，是否小于窗口的左端点，如果小于则弹出！！
+		存放两个Map，一个是用于存放窗口的值，一个是用于存放需要的字符串，以及每个字符出现的次数！！
+		valid用于判断这个字符以及出现的次数，是否满足need中对应的字符和出现的次数，如果满足则valid++,当valid为need的长度，说明要处理窗口了！
 		
-题解：https://leetcode-cn.com/problems/sliding-window-maximum/solution/shuang-xiang-dui-lie-jie-jue-hua-dong-chuang-kou-2/
+题解：https://leetcode-cn.com/problems/minimum-window-substring/solution/labuladongde-suan-fa-xiao-chao-javaban-b-glhm/
 
 */
 
 
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        if(k == 0 || nums.length == 0)
-            return new int[0];
+    public String minWindow(String s, String t) {
+        HashMap<Character,Integer> need = new HashMap();
+        HashMap<Character,Integer> window = new HashMap();
+        //need存放的不重复的字符
+        for(char c:t.toCharArray())
+            need.put(c,need.getOrDefault(c,0)+1);
+        
+        //left,right 表示滑动窗口的左右指针
+        int left = 0 , right = 0;
+        //valid表示是否满足了t中的字符，不算重复的
+        int valid = 0;
+        //记录最小覆盖子串的起始索引及长度
+        int start = 0 , len = Integer.MAX_VALUE;
+        while(right < s.length()){
+            char c = s.charAt(right);
+            right++;
+            //判断取出的字符是否在需要的Map中
+            if(need.containsKey(c)){
+                window.put(c,window.getOrDefault(c,0)+1);
+                if(window.get(c).equals(need.get(c)))
+                    valid++;
+            }
 
-        if(k == 1)
-            return nums;
+            //判断是否需要收缩（即已经找到了合适的覆盖串）
+            while(valid == need.size()){
+                //更新最小覆盖子串
+                if(right - left < len){
+                    start = left;
+                    len = right - left;
+                }
 
-        Deque<Integer> queue = new LinkedList();
-        List<Integer> list = new ArrayList();
+                char c1 = s.charAt(left);
+                //左移窗口
+                left++;
 
-        //遍历nums数组
-        for(int i=0;i<nums.length;i++){
-            //保证从大到小的队列
-            while(!queue.isEmpty() && nums[queue.peekLast()] <= nums[i])
-                queue.pollLast();
-            //将当前值的下标添加到queue中
-            queue.addLast(i);
-            //判断队首的值是否过期了（即该下标值已经被移除了）
-            
-            //此时已经形成了窗口
-            if(i+1 >= k){
-                list.add(nums[queue.peek()]);
-                //判断当前队头存放的下标值是否已经过期
-                //i+1-k  对应的是滑动窗口的左边界
-                if(queue.peek() <= (i+1-k))
-                    queue.poll();
+                //进行窗口内数据的一系列更新
+                //如果当前要移动的字符是包含在need中，我们需要进行讨论，如果该字符的次数刚好与我们需要的次数相等，则valid--，并同时更新window中这个值出现的次数
+                if(need.containsKey(c1)){
+                    if(window.get(c1).equals(need.get(c1)))
+                        valid--;
+                    window.put(c1,window.getOrDefault(c1,0)-1);
+                }
             }
         }
-
-        int[] result = new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            result[i] = list.get(i);
-        }
-        return result;
-    }
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
+    }   
 }
