2050cc4e05fbcdedf0760da448bbd75eff6c0631
TerrellTong <terrell_tong@126.com>
2021-01-08 16:22:10 +0800

    41 113.路径总和 II_回溯.java

--- java/字节跳动/41 113.路径总和 II_回溯.java
+++ java/字节跳动/41 113.路径总和 II_回溯.java
@@ -27,7 +27,11 @@ class Solution {
         cur.add(root.val);
         if(res == sum && root.left == null && root.right == null){
             result.add(new ArrayList(cur));
+            //注意别忘了把最后加入的结点值(上面的cur.add)给移除掉，因为下一步直接return了，
+            //不会再走最后一行的remove了，所以这里在rerurn之前提前把最后
+            //一个结点的值给remove掉。
             cur.remove(cur.size()-1);
+            res -= root.val;
             return;
         }
         //不能直接返回，因为会出现负数！！
