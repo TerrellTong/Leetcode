7b966c0e1c86bbb97616e6f631d6ad42fad3b732
TerrellTong <terrell_tong@126.com>
2021-02-04 20:19:23 +0800

    652.寻找重复的子树_labuladong.java_整合自己的序列化代码，没有用labuladong的思想

--- java/Day30题目/652.寻找重复的子树_labuladong.java
+++ java/Day30题目/652.寻找重复的子树_labuladong.java
@@ -22,6 +22,35 @@
  *     }
  * }
  */
+ 
+//按照先序的思想，先序的序列化，不要按照labuladong的思想将结果传入str，直接返回即可！
+class Solution {
+    Map<String,Integer> set = new HashMap();
+    List<TreeNode> res = new ArrayList();
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        serialize(root);
+        return res;
+    }
+    
+    //重复子树的定义：二叉树的序列化的结果不重复,通过Map判断，我想的还是定一个，从而转到另一个！
+    //二叉树的模样：通过序列化即可，将null用#代替，用‘，’分割二叉树的节点值
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "None,";
+        }
+        String str = root.val +',' + serialize(root.left) + serialize(root.right);
+
+        if(set.getOrDefault(str,0)==1){
+            res.add(root);
+        }
+
+        set.put(str,set.getOrDefault(str,0)+1);
+
+        return str;
+    }
+} 
+ 
+//labuladong的思想
 class Solution {
     Map<String,Integer> map = new HashMap();
     List<TreeNode> res = new ArrayList();
@@ -50,3 +79,5 @@ class Solution {
         return result;
     }
 }
+
+
